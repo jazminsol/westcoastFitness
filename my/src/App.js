03a03595e6d8660c06f5ec7b3cd479ca0027dc1c
Justin <jdanielmcarthur@msn.com>
2021-06-21 14:40:00 -0700

    created LoginForm component, with a simple username and password input field

---
+++ my/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LoginForm from "./LoginForm"
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
+
+
