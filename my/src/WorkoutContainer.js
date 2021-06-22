54b6a589d05252f10af5b596357b11ea6b2e193c
jazmin <jazminsol@yahoo.com>
2021-06-22 15:43:51 -0500

    fetch

--- my/src/WorkoutContainer.js
+++ my/src/WorkoutContainer.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function WorkoutContainer() {
+
+    const [workoutSection, setWorkouts] = useState([]);
+    useEffect (() => {
+        fetch('http://localhost:3000/workouts/')
+        .then(response => response.json())
+        .catch(error => console.error('Error:', error))
+        .then(data => setWorkouts(data));
+    }, []);
+   
     return (
         <div>
             <h1>Welcome back, "user"!</h1>
-        </div>
+            {workoutSection.map((workouts) => <div> {workouts.name} </div> )}
+    </div>
     )
 }
