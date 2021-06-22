31d9a7290d59167bc0fd2045948113a276883da3
Justin <jdanielmcarthur@msn.com>
2021-06-22 10:38:55 -0700

    installed react-router-dom, created the workoutcontainer component, imported it into App

--- my/src/App.js
+++ my/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LoginForm from "./LoginForm"
+import WorkoutContainer from "./WorkoutContainer"
 
 function App() {
   return (
     <div className="App">
       <LoginForm />
+      <WorkoutContainer />
     </div>
   );
 }
