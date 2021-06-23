e33562936ef1d1ed76e380326ca46a1456483864
Justin <jdanielmcarthur@msn.com>
2021-06-22 23:08:46 -0700

    added an email input on loginForm, rendered cards using semantic ui

--- my/src/CardBox.js
+++ my/src/CardBox.js
@@ -1,19 +1,13 @@
 import React from 'react'
+import Cards from "./Card"
+
 
 function CardBox( {workoutCollection } ) {
 
     return (
      <div className="card-box"> 
-            {workoutCollection.map(workout => 
-               <div key={workout.name} className="workout-card">
-                   <img className="card-image" alt="workout" src={workout.image} />
-                   <hr></hr>
-                   <h5>Activity: {workout.name}</h5>
-                   <h5>Level: {workout.difficulty}</h5>
-                   <h5>Time Spent:{workout.duration}</h5>
-               </div>
-            )}
-        </div>
+        <Cards workoutCollection={workoutCollection}/>
+     </div>
     )
 }
 
